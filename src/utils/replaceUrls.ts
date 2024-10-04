004435f6649dc835a314e983a1e01504abd7dee2
uchikori <ucchii.wa@gmail.com>
2024-10-04 11:59:24 +0900

    セクション4.32まで

--- src/utils/replaceUrls.ts
+++ src/utils/replaceUrls.ts
@@ -1,3 +1,4 @@
 export const replaceUrls = (content: string, replacementUrl: string) => {
+  //contentの中に含まれるPUBLIC_WP_URL文字列をreplacementUrlに置き換える
   return content.replaceAll(`${import.meta.env.PUBLIC_WP_URL}`, replacementUrl);
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,7 @@ export default {
     extend: {
       colors: {
         "event-horizon": "var(--color--event-horizon)",
+        "blast-off": "var(--color--blast-off)",
       },
     },
   },
